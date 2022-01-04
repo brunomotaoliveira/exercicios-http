6b5061865be2ed0a5d0d6161b758b89a45bdaa03
brunomotadev <brunomotadev@gmail.com>
2022-01-04 14:27:26 -0300

    consumindo api de cep

---
+++ app/Http/Controllers/CorreiosController.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use App\Models\Cluster;
+use Illuminate\http\Client\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class CorreiosController extends Controller {
+    public function buscardados() {
+        
+
+        $urlBusca = "https://viacep.com.br/ws/01001000/json/unicode/";
+
+        $response = Http::get($urlBusca);
+
+        $dadosDaBusca = $response->json();
+
+        
+        return response($dadosDaBusca['ibge']);
+        
+    }
+
+    
+}
+
+?>
