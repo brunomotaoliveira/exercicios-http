32926b74def1055a17eeea90707e5f79ad6f5501
brunomotadev <brunomotadev@gmail.com>
2022-01-06 12:26:11 -0300

    alterado os valores

--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -9,17 +9,17 @@ class ProdutosController extends Controller
 {
     public static function getPriceInProductVtex($id) {
         $urlProductId = "https://casaevideo.corebiz.ag/vtexcommercestable/api/catalog_system/pub/products/search?fq=productId:$id";
-        $responseProdutoUm = Http::get($urlProductId);
-        $valorProdutoUm = $responseProdutoUm->json();
+        $response = Http::get($urlProductId);
+        $priceProduct = $response->json();
 
-        return $valorProdutoUm[0]["items"][0]["sellers"][0]["commertialOffer"]["Price"];
+        return $priceProduct[0]["items"][0]["sellers"][0]["commertialOffer"]["Price"];
     }
 
     public function somaDosProdutos() 
     {
         $produtoUm = ProdutosController::getPriceInProductVtex(1000); // this->getPriceInProductVtex(1000);
         $produtoDois = ProdutosController::getPriceInProductVtex(20445); // tem que estar static
-        $produtoTres = self::getPriceInProductVtex(1000);
+        //$produtoTres = self::getPriceInProductVtex(1000); -> terceira forma de fazer
         dd($produtoUm + $produtoDois);
     }
 }
