03ccd20a14f11bfd72cb27206a2cd8c923f371d5
brunomotadev <brunomotadev@gmail.com>
2022-01-11 18:43:03 -0300

    criado a function setSoma

--- app/Http/Controllers/CalculadoraController.php
+++ app/Http/Controllers/CalculadoraController.php
@@ -7,4 +7,10 @@ use Illuminate\Http\Request;
 class CalculadoraController extends Controller
 {
     //
+    public function setSoma(Request $request) {
+        $somaValor1 = $request->input('valor1');
+        $somaValor2 = $request->input('valor2');
+    }
+
+    public function setSubtracao(Request $request)
 }
